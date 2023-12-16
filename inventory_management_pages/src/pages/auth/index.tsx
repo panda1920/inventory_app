dc83863a0297d0f3cd45dbc2d2064d9c25a452c9
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2023-12-17 02:03:27 +0900

    improved error handling and validation in /auth

--- inventory_management_pages/src/pages/auth/index.tsx
+++ inventory_management_pages/src/pages/auth/index.tsx
@@ -1,13 +1,23 @@
+import { ParsedUrlQuery } from 'querystring'
+
 import { CircularProgress } from '@mui/material'
 import { applyActionCode, signInWithCredential } from 'firebase/auth'
 import { useRouter } from 'next/router'
 import { useSnackbar } from 'notistack'
 import { useEffect } from 'react'
 
+import { InventoryAppClientError } from '@/helper/errors'
 import { getFirebaseAuth, getInitializedFirebaseAuth } from '@/helper/firebase'
 import { withServerSideHooks } from '@/helper/serverside-hooks'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { clearTemporaryCredentialAction, getTemporaryCredential } from '@/store/slice/user'
+
+type FirebaseAuthEmailHandlerParams = {
+  mode: 'resetPassword' | 'recoverEmail' | 'verifyEmail'
+  oobCode: string
+  apiKey: string
+  continueUrl: string
+}
 
 /**
  * A page to handle firebase auth email action
@@ -18,45 +28,41 @@ export default function Auth() {
   const { enqueueSnackbar } = useSnackbar()
   const dispatch = useAppDispatch()
   const temporaryCredential = useAppSelector(getTemporaryCredential)
-  const { mode, oobCode, apiKey, continueUrl } = router.query
-  console.log(
-    '🚀 ~ file: index.tsx:22 ~ Auth ~ mode, oobCode, apiKey, continueUrl:',
-    mode,
-    oobCode,
-    apiKey,
-    continueUrl,
-  )
 
   useEffect(() => {
     const handleEmailAction = async () => {
-      // basic parameter check
-      if (!isParametersValid(mode, oobCode, apiKey, continueUrl)) {
-        console.error('Invalid params')
-        return router.replace('/')
-      }
-
-      if (mode === 'verifyEmail') {
-        // TODO: think about delegating this whole chunk to a hook
-        // TODO: error handling
-        // TODO: think about how to verify email login accounts
-        // verify code
-        await applyActionCode(getFirebaseAuth(), oobCode as string)
-        toastAccountVerified()
-
-        // TODO: need to login to backend as well
-        if (temporaryCredential) {
-          await signInWithCredential(getInitializedFirebaseAuth(), temporaryCredential)
-          dispatch(clearTemporaryCredentialAction())
-          toastLoginSuccess()
+      try {
+        // basic parameter check
+        if (!isValidFirebaseAuthEmailHandlerParams(router.query)) {
+          throw new InventoryAppClientError('Invalid params')
         }
+        const { mode, oobCode, continueUrl } = router.query
 
-        // TODO: handle continueUrl with more flexibility
-        router.push(continueUrl as string)
+        if (mode === 'verifyEmail') {
+          // TODO: think about delegating this whole chunk to a hook
+          // TODO: think about how to verify email login accounts
+          // verify code
+          await applyActionCode(getFirebaseAuth(), oobCode as string)
+          toastAccountVerified()
+
+          // TODO: need to login to backend as well
+          if (temporaryCredential) {
+            await signInWithCredential(getInitializedFirebaseAuth(), temporaryCredential)
+            dispatch(clearTemporaryCredentialAction())
+            toastLoginSuccess()
+          }
+
+          router.push(continueUrl as string)
+        }
+      } catch (e) {
+        console.error(e)
+        return router.replace('/')
       }
     }
 
     handleEmailAction()
-  })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   function toastAccountVerified() {
     enqueueSnackbar('Your account has been verified.', { variant: 'success' })
@@ -68,10 +74,27 @@ export default function Auth() {
   return <CircularProgress />
 }
 
-function isParametersValid(mode: any, oobCode: any, apiKey: any, continueUrl: any) {
-  if (mode !== 'resetPassword' || mode !== 'recoverEmail' || mode !== 'verifyEmail') return false
+function isValidFirebaseAuthEmailHandlerParams(
+  queryParams: ParsedUrlQuery,
+): queryParams is FirebaseAuthEmailHandlerParams {
+  const { mode, oobCode, apiKey, continueUrl } = queryParams
 
-  if (!oobCode || !apiKey || !continueUrl) return false
+  // should not be undefined or string[]
+  if (!mode || !oobCode || !apiKey || !continueUrl) return false
+  if (
+    Array.isArray(mode) ||
+    Array.isArray(oobCode) ||
+    Array.isArray(apiKey) ||
+    Array.isArray(continueUrl)
+  )
+    return false
+
+  // should be a certain mode
+  if (!['resetPassword', 'recoverEmail', 'verifyEmail'].find((_mode) => _mode === mode))
+    return false
+
+  // should be website url
+  if (!continueUrl.startsWith(process.env.NEXT_PUBLIC_SITE_URL || '')) return false
 
   return true
 }
