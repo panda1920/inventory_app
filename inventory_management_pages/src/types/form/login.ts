89c3620807f3ba887a4a3bce1730e16b52b7caf5
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2023-12-24 12:02:55 +0900

    Refactored zod schema for DRYness

--- inventory_management_pages/src/types/form/login.ts
+++ inventory_management_pages/src/types/form/login.ts
@@ -1,15 +1,10 @@
 import { z } from 'zod'
 
+import { baseSignupSchema } from '@/types/form/signup'
+
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z
-    .string()
-    .min(8, { message: 'Password must be at least 8 characters long.' })
-    .refine((password) => {
-      if (!password.match(/[a-zA-Z]/)) return false
-      // if (!password.match(/[0-9]/)) return false
-      return true
-    }, 'Password must contain atleast 1 numeric and 1 alphabetical character.'),
+  email: baseSignupSchema.shape.email,
+  password: baseSignupSchema.shape.password,
 })
 
 export type LoginSchema = z.infer<typeof loginSchema>
