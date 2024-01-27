873265466aacca47066b2972b246673a85c90caa
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2024-01-28 04:46:29 +0900

    somewhat implemented items page

--- inventory_management_pages/src/pages/items/index.tsx
+++ inventory_management_pages/src/pages/items/index.tsx
@@ -1,24 +1,78 @@
-import { Typography } from '@mui/material'
+import Add from '@mui/icons-material/Add'
+import Delete from '@mui/icons-material/Delete'
+import Edit from '@mui/icons-material/Edit'
+import Remove from '@mui/icons-material/Remove'
+import { Box, Checkbox, Typography, useTheme } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 import { listItems } from '@/handlers/item'
+import { InventoryAppClientError } from '@/helper/errors'
 import { withServerSideHooks } from '@/helper/serverside-hooks'
 import type { Item } from '@/types/entity/item'
+import { UpdateItemSchema } from '@/types/form/item'
 
 type ItemsProps = {
   items: Item[]
 }
 
-export default function Items({ items }: ItemsProps) {
+export default function Items({ items: fetchedItems }: ItemsProps) {
+  const theme = useTheme()
+  const [items, setItems] = useState(fetchedItems)
+  const [itemsChecklist, setItemsChecklist] = useState(createNewItemsChecklist(items))
+
+  useEffect(() => {
+    setItemsChecklist(createNewItemsChecklist(items))
+  }, [items])
+
+  const createAddQuantity = (itemIndex: number) => {
+    return async (toAdd: number) => {
+      const item = items[itemIndex]
+
+      try {
+        await updateItem(item.id!, { quantity: item.quantity + toAdd })
+        item.quantity += toAdd
+        setItems([...items.slice(0, itemIndex), item, ...items.slice(itemIndex + 1)])
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+
+  const createDeleteItem = (itemIndex: number) => {
+    return async () => {
+      const item = items[itemIndex]
+
+      try {
+        await deleteItem(item.id!)
+        setItems([...items.slice(0, itemIndex), ...items.slice(itemIndex + 1)])
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+
+  const onItemChecked = (isChecked: boolean, i: number) => {
+    setItemsChecklist([...itemsChecklist.slice(0, i), isChecked, ...itemsChecklist.slice(i + 1)])
+  }
+
   return (
     <section>
-      <h1>User</h1>
-      <ul>
-        {items.map((item) => (
+      <h1 className='text-center' style={{ marginBlock: theme.spacing(2) }}>
+        <Typography variant='h3'>Your Items</Typography>
+      </h1>
+      <ul
+        className={`ms-[auto] me-[auto]`}
+        style={{ width: `min(1000px, 100% - ${theme.spacing(2)})` }}
+      >
+        {items.map((item, index) => (
           <li key={item.id}>
-            <Typography>
-              Item: {item.name} * {item.quantity}
-            </Typography>
-            <Typography>Created at: {item.createdAt}</Typography>
+            <ItemInList
+              item={item}
+              isChecked={itemsChecklist[index]}
+              onChecked={(isChecked, _) => onItemChecked(isChecked, index)}
+              addQuantity={createAddQuantity(index)}
+              deleteItem={createDeleteItem(index)}
+            />
           </li>
         ))}
       </ul>
@@ -29,7 +83,7 @@ export default function Items({ items }: ItemsProps) {
 export const getServerSideProps = withServerSideHooks<ItemsProps>(async (context) => {
   let items: Item[] = []
   if (context.user) {
-    items = await listItems({ limit: 10, ownerId: context.user.uid })
+    items = await listItems({ limit: 100, ownerId: context.user.uid })
   }
 
   return {
@@ -38,3 +92,94 @@ export const getServerSideProps = withServerSideHooks<ItemsProps>(async (context
     },
   }
 })
+
+function createNewItemsChecklist(items: Item[]) {
+  return Array<boolean>(items.length).fill(false)
+}
+
+function ItemInList({
+  item,
+  isChecked,
+  onChecked,
+  addQuantity,
+  deleteItem,
+}: {
+  item: Item
+  isChecked: boolean
+  onChecked: (isChecked: boolean, item: Item) => void
+  addQuantity: (toAdd: number) => void
+  deleteItem: () => Promise<void>
+}) {
+  const theme = useTheme()
+
+  return (
+    <Box
+      className='grid justify-items-start items-center text-white'
+      style={{
+        gridTemplateColumns: theme.spacing('auto', '1fr', 6, 'auto'),
+        gap: theme.spacing(1),
+        padding: theme.spacing(0, 2),
+      }}
+    >
+      <Checkbox
+        onChange={(_, isChecked) => onChecked(isChecked, item)}
+        checked={isChecked}
+        color='primary'
+      />
+      <Typography>{item.name}</Typography>
+      <Typography className='justify-self-end'>{item.quantity}</Typography>
+      <Box className='flex flex-row items-center text-2xl'>
+        <Add
+          fontSize='inherit'
+          className='!transition-transform hover:scale-125 hover:cursor-pointer'
+          onClick={() => addQuantity(1)}
+        />
+        <Remove
+          fontSize='inherit'
+          className='!transition-transform hover:scale-125 hover:cursor-pointer'
+          onClick={() => addQuantity(-1)}
+        />
+        <Edit
+          fontSize='inherit'
+          className='!transition-transform hover:scale-125 hover:cursor-pointer'
+        />
+        <Delete
+          fontSize='inherit'
+          className='!transition-transform hover:scale-125 hover:cursor-pointer'
+          onClick={deleteItem}
+        />
+      </Box>
+    </Box>
+  )
+}
+
+async function updateItem(id: string, update: UpdateItemSchema) {
+  const url = `/api/items/${id}`
+  const options = {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(update),
+  } satisfies RequestInit
+
+  const response = await fetch(url, options)
+  if (response.ok) return await response.json()
+
+  console.error(response.body)
+  console.error(response.status)
+  throw new InventoryAppClientError('Item update failed')
+}
+
+async function deleteItem(id: string) {
+  const url = `/api/items/${id}`
+  const options = {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  } satisfies RequestInit
+
+  const response = await fetch(url, options)
+  if (response.ok) return await response.json()
+
+  console.error(response.body)
+  console.error(response.status)
+  throw new InventoryAppClientError('Item delete failed')
+}
