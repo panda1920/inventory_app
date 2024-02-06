3d8f0c75874e335f4f3e32d6ead06d5c8fcf2eee
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2024-02-07 01:39:45 +0900

    [ADD] /auth/check endpoint to verify session

---
+++ inventory_management_pages/src/pages/api/auth/check.ts
@@ -0,0 +1,26 @@
+import { HandlerSpecByMethods, createCommonApiHandler } from '@/helper/api'
+import { auth } from '@/helper/firebase-admin'
+import { FailResponse, SuccessResponse } from '@/types/api/common'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = SuccessResponse | FailResponse
+
+// a simple API endpoint that checks validity of session cookie value
+const checkHandler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const token = req.query.token
+  if (!token || Array.isArray(token))
+    return res.status(401).json({ success: false, message: 'Invalid' })
+
+  try {
+    await auth.verifySessionCookie(token)
+    res.status(200).json({ success: true })
+  } catch (e) {
+    res.status(401).json({ success: false, message: 'Invalid' })
+  }
+}
+
+const handlers = {
+  GET: { handler: checkHandler },
+} satisfies HandlerSpecByMethods
+
+export default createCommonApiHandler(handlers)
