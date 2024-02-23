563df43513ea0fe9346f76bdd09bb2f18be590d7
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2024-02-23 22:46:24 +0900

    fixed content of index

--- inventory_management_pages/src/pages/index.tsx
+++ inventory_management_pages/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+import { Box, Button } from '@mui/material'
 import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
@@ -11,9 +11,7 @@ export default function Home() {
   const router = useRouter()
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <Box className={`items-center justify-between p-24 ${inter.className}`}>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         {/* <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
           Get started by editing&nbsp;
@@ -50,7 +48,7 @@ export default function Home() {
         />
       </div> */}
 
-      <div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
+      <div className='grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
         <a
           href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app'
           className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
@@ -130,7 +128,7 @@ export default function Home() {
           Go to items
         </Button>
       </div>
-    </main>
+    </Box>
   )
 }
 
