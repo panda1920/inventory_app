3516532b31cced7ef37f5895981a3d2dae8c61b8
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2024-02-24 21:47:03 +0900

    Fixed apperance of signup modal as per login

--- inventory_management_pages/src/components/modal/signup/signup-modal.tsx
+++ inventory_management_pages/src/components/modal/signup/signup-modal.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Typography, useTheme } from '@mui/material'
+import Image from 'next/image'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -7,6 +8,8 @@ import Input from '@/components/form/input'
 import InlineButton from '@/components/inline-button/inline-button'
 import CommonModal from '@/components/modal/common/common-modal'
 import { useAuth } from '@/hooks/auth'
+import { useAppSelector } from '@/store/hooks'
+import { getColorScheme } from '@/store/slice/app'
 import { SignupSchema, signupSchema } from '@/types/form/signup'
 
 type SignupModalProps = {
@@ -34,6 +37,8 @@ export default function SignupModal({ isOpen, close, openLogin }: SignupModalPro
       confirm: '',
     },
   })
+  const theme = useTheme()
+  const colorScheme = useAppSelector(getColorScheme)
 
   // reset form state when modal opens
   useEffect(() => {
@@ -46,63 +51,99 @@ export default function SignupModal({ isOpen, close, openLogin }: SignupModalPro
 
   return (
     <CommonModal isOpen={isOpen} close={close} title='Signup'>
-      <Box component='form' onSubmit={handleSubmit(signupHandler)}>
-        <Typography>Signup with email</Typography>
-        <Input
-          label='Username'
-          name='username'
-          type='text'
-          autoComplete='username'
-          control={control}
-        />
-        <Input label='Email' name='email' type='email' autoComplete='email' control={control} />
-        <Input
-          label='Password'
-          name='password'
-          type='password'
-          autoComplete='new-password'
-          control={control}
-        />
-        <Input
-          label='Confirm password'
-          name='confirm'
-          type='password'
-          autoComplete='new-password'
-          control={control}
-        />
-        <Button
-          type='submit'
-          color='primary'
-          disableElevation={true}
-          sx={{ textAlign: 'center' }}
-          disabled={isSubmitting}
+      <Box className='flex flex-col' sx={{ gap: { xs: theme.spacing(3), sm: theme.spacing(4) } }}>
+        <Box
+          component='form'
+          onSubmit={handleSubmit(signupHandler)}
+          className='flex flex-col'
+          style={{ gap: theme.spacing(0.5) }}
         >
-          SIGNUP
-        </Button>
-      </Box>
+          <Input
+            label='Username'
+            name='username'
+            type='text'
+            autoComplete='username'
+            variant='standard'
+            control={control}
+          />
+          <Input
+            label='Email'
+            name='email'
+            type='email'
+            autoComplete='email'
+            variant='standard'
+            control={control}
+          />
+          <Input
+            label='Password'
+            name='password'
+            type='password'
+            autoComplete='new-password'
+            variant='standard'
+            control={control}
+          />
+          <Input
+            label='Confirm password'
+            name='confirm'
+            type='password'
+            autoComplete='new-password'
+            variant='standard'
+            control={control}
+          />
+          <Button
+            type='submit'
+            color='primary'
+            variant='contained'
+            className='self-start'
+            style={{ marginBlock: theme.spacing(1) }}
+            disabled={isSubmitting}
+          >
+            SIGNUP
+          </Button>
+        </Box>
 
-      <Box>
-        <Button
-          onClick={loginWithGoogleHandler}
-          disableElevation={true}
-          sx={{ textAlign: 'center' }}
-        >
-          <Typography>Signup with Google</Typography>
-        </Button>
-        <Button
-          onClick={loginWithGithubHandler}
-          disableElevation={true}
-          sx={{ textAlign: 'center' }}
-        >
-          <Typography>Signup with Github</Typography>
-        </Button>
-      </Box>
+        <Box className='flex flex-col' style={{ gap: theme.spacing(2) }}>
+          <Typography>Signup with the following providers:</Typography>
+          <Box className='flex flex-row items-baseline' style={{ gap: theme.spacing(2) }}>
+            <Button
+              onClick={loginWithGoogleHandler}
+              disableElevation={true}
+              className='!p-0 !min-w-0 hover:opacity-80'
+            >
+              <Image
+                src='/assets/google.svg'
+                alt='Google'
+                width='36'
+                height='36'
+                className='w-[36px] h-auto'
+              />
+            </Button>
+            <Button
+              onClick={loginWithGithubHandler}
+              disableElevation={true}
+              className='!p-0 !min-w-0 hover:opacity-80'
+            >
+              <Image
+                src={
+                  colorScheme === 'light'
+                    ? '/assets/github-mark.svg'
+                    : '/assets/github-mark-white.svg'
+                }
+                alt='Github'
+                width='36'
+                height='36'
+                className='w-[36px] h-auto'
+              />
+            </Button>
+          </Box>
+        </Box>
 
-      <Box>
-        <Typography>
-          Already have an account? <InlineButton onClick={openLogin}>Login</InlineButton> to the
-          app!
-        </Typography>
+        <Box>
+          <Typography>
+            Already have an account? <InlineButton onClick={openLogin}>Login</InlineButton> to the
+            app!
+          </Typography>
+        </Box>
       </Box>
     </CommonModal>
   )
