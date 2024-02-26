686901e41667d230a457430a77d9a2c13b25cf6a
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2024-02-27 00:35:37 +0900

    Added header logo image

--- inventory_management_pages/src/components/header/header.tsx
+++ inventory_management_pages/src/components/header/header.tsx
@@ -4,12 +4,15 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import clsx from 'clsx'
+import Image from 'next/image'
 import { MouseEvent, useState } from 'react'
 
 import DropdownMenu from '@/components/header/dropdown-menu'
 import LoginModal from '@/components/modal/login/login-modal'
 import SignupModal from '@/components/modal/signup/signup-modal'
 import { useAuth } from '@/hooks/auth'
+import { useAppSelector } from '@/store/hooks'
+import { getColorScheme } from '@/store/slice/app'
 
 type HeaderType = {
   className?: string
@@ -22,6 +25,7 @@ export default function Header({ className, contentClassName }: HeaderType) {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
   const { logoutFromBackend } = useAuth({ afterLogoutAction: closeMenu })
   const theme = useTheme()
+  const colorScheme = useAppSelector(getColorScheme)
 
   function openMenu(event: MouseEvent<HTMLElement>) {
     setAnchorEl(event.currentTarget)
@@ -43,7 +47,12 @@ export default function Header({ className, contentClassName }: HeaderType) {
   return (
     <AppBar position='static' className={className}>
       <Toolbar className={clsx('flex flex-row justify-between py-4 !px-0', contentClassName)}>
-        <Typography variant='h3'>Header</Typography>
+        <Image
+          src={colorScheme === 'light' ? '/assets/logo.svg' : '/assets/logo-white.svg'}
+          height='36'
+          width='222' // 6.19 * height
+          alt='logo'
+        />
 
         <IconButton aria-label='hamburger-menu' size='medium' onClick={openMenu}>
           <MenuIcon fontSize='inherit' htmlColor={theme.palette.primary.contrastText} />
